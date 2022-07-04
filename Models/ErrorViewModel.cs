0169805fb3cc8759097811e1d3f9cc40847b3aa9
ajike112 <101846555+ajike112@users.noreply.github.com>
2022-07-04 16:07:58 -0400

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UDEMYNEWDOTNETWEBAPP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
